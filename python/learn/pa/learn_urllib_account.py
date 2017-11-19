b9ff4a9f31a1ea0d24dbcffdee88067e8277e9c1
vivian_sun <work_man2012@126.com>
2017-11-19 19:13:11 +0800

    modify lib

--- python/learn/pa/learn_urllib_account.py
+++ python/learn/pa/learn_urllib_account.py
@@ -2,14 +2,17 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import urllib
 import urllib2
 
 def main():
     reload(sys)
     sys.setdefaultencoding('utf-8')
 
-    values = {"username": "randy_wang@asus.com", "password": "asus@123"}
-    data = urllib2.urlencode(values)
+    values = {}
+    values['username'] = "1016903103@qq.com"
+    values['password'] = "XXXX"
+    data = urllib.urlencode(values)
     url = "http://passport.umeng.com/login"
     request = urllib2.Request(url, data)
     response = urllib2.urlopen(request)
