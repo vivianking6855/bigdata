8dec88bcb62d6b0014fa5ee84976a54a06abd8ee
vivian_sun <vivian_sun@asus.com>
2017-11-20 11:25:09 +0800

    udpate

--- python/learn/pa/4_learn_urllib_header.py
+++ python/learn/pa/4_learn_urllib_header.py
@@ -1,5 +1,6 @@
 #!/usr/local/bin/python
 # -*- coding: utf-8 -*-
+# learn from Python爬虫入门四之Urllib库的高级用法 http://cuiqingcai.com/954.html
 
 import sys
 import urllib
@@ -10,15 +11,22 @@ def main():
     reload(sys)
     sys.setdefaultencoding('utf-8')
 
-    url = 'http://www.server.com/login'
-    user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
-    values = {'username': 'cqc', 'password': 'XXXX'}
-    headers = {'User-Agent': user_agent}
-    data = urllib.urlencode(values)
-    request = urllib2.Request(url, data, headers)
-    response = urllib2.urlopen(request)
-    page = response.read()
-    print page
+    url = 'http://www.santostang.com/'
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'}
+    # values = {'username': 'cqc', 'password': 'XXXX'}
+    # data = urllib.urlencode(values)
+    # request = urllib2.Request(url, data, headers)
+    request = urllib2.Request(url, None, headers)
+
+    try:
+        response = urllib2.urlopen(request)
+        page = response.read()
+        print page
+    except urllib2.HTTPError, e:
+        print e.code
+        print e.reason
+    except TypeError, e:
+        print e.message
 
 
 if __name__ == '__main__':
